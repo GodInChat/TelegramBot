807ca321b203e19c7cebc5470162e1ff7fc91194
Andrii <123271442+DarthRomanov@users.noreply.github.com>
2023-12-27 22:38:39 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,32 +1,63 @@
-import telebot
+import asyncio
+import logging
+import sys
+from os import getenv
 
-bot = telebot.TeleBot("6687358452:AAGr0lOmafiFeFthkzkpMq8QnoLuoM8REKE")
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
 
-# Показати список команд
-def show_commands(message):
-    bot.reply_to(message, "/upload - завантажити файл контексту")
-    bot.reply_to(message, "/delete - видалити файл контексту")
-    bot.reply_to(message, "/ask - задати питання")
-    bot.reply_to(message, "/history - переглянути історію запитань")
-    bot.reply_to(message, "/clear_history - очистити історію запитань")
-    bot.reply_to(message, "/exit - завершити роботу")
-    bot.reply_to(message, "/restart - рестарт спілкування")
+# Bot token can be obtained via https://t.me/BotFather
+TOKEN = "6687358452:AAGr0lOmafiFeFthkzkpMq8QnoLuoM8REKE"
 
-# Обробник команди /start
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, "Вас вітає бот. Оберіть команду:")
-    show_commands(message)
+# All handlers should be attached to the Router (or Dispatcher)
+dp = Dispatcher()
 
-# Обробник команди /upload
-@bot.message_handler(commands=['upload'])
-def upload_file(message):
-    # Реалізуйте логіку завантаження файлу тут
-    bot.reply_to(message, "Виберіть файл для завантаження.")
 
-# Обробник команди /restart
-@bot.message_handler(commands=['restart'])
-def restart_conversation(message):
-    bot.reply_to(message)
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    # Most event objects have aliases for API methods that can be called in events' context
+    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
+    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
+    # method automatically or call API method directly via
+    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
+    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
+    await message.answer(f"/upload - завантажити файл контексту")
+    await message.answer(f"/delete - видалити файл контексту")
+    await message.answer(f"/ask - задати питання")
+    await message.answer(f"/history - переглянути історію запитань")
+    await message.answer(f"/clear_history - очистити історію запитань")
+    await message.answer(f"/exit - завершити роботу")
+    await message.answer(f"/restart - рестарт спілкування")
 
-bot.infinity_polling()
+
+@dp.message()
+async def echo_handler(message: types.Message) -> None:
+    """
+    Handler will forward receive a message back to the sender
+
+    By default, message handler will handle all message types (like a text, photo, sticker etc.)
+    """
+    try:
+        # Send a copy of the received message
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        # But not all the types is supported to be copied so need to handle it
+        await message.answer("Nice try!")
+
+
+async def main() -> None:
+    # Initialize Bot instance with a default parse mode which will be passed to all API calls
+    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
